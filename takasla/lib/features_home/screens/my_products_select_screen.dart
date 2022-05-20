9b72834149f2a0c7396043ce9dba87b62541ed68
Taha Can <taha.can@metu.edu.tr>
2022-05-20 18:35:42 +0300

    updated-app-photos

--- takasla/lib/features_home/screens/my_products_select_screen.dart
+++ takasla/lib/features_home/screens/my_products_select_screen.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:takasla/main/constants.dart';
 
 import '../../main/ui_components.dart';
+import 'dart:io';
+import 'dart:convert';
+
+import 'package:intl/intl.dart';
 
 class MyProductScreen extends StatefulWidget {
   const MyProductScreen({Key? key}) : super(key: key);
@@ -11,6 +15,10 @@ class MyProductScreen extends StatefulWidget {
 }
 
 class _MyProductScreenState extends State<MyProductScreen> {
+  String dropdownValue = 'Ankara Teslimat Merkezi';
+  DateTime? _dateTime;
+  dynamic dodString = "";
+  late String dodToSend;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,61 +52,178 @@ class _MyProductScreenState extends State<MyProductScreen> {
                     )
                   ],
                 ),
-                   ListView(
-                          children:[
-                          Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                            Container(child: Text('asasa')),
-                        ],
-                      ),
+                Expanded(
+                    child: ListView(
+                  children: [
+                    GridView.count(
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      scrollDirection: Axis.vertical,
+                      padding: EdgeInsets.all(8),
+                      childAspectRatio: 0.55,
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 12,
+                      mainAxisSpacing: 12,
+                      children: [
+                        CustomCard(
+                            onPressed: () {
+                              print('Ürün');
+                            },
+                            text: 'Ürün',
+                            imageUrl:
+                                'https://images.pexels.com/photos/3731256/pexels-photo-3731256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+                        CustomCard(
+                            onPressed: () {
+                              print('Ürün');
+                            },
+                            text: 'Ürün',
+                            imageUrl:
+                                'https://images.pexels.com/photos/3731256/pexels-photo-3731256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+                        CustomCard(
+                            onPressed: () {
+                              print('Ürün');
+                            },
+                            text: 'Ürün',
+                            imageUrl:
+                                'https://images.pexels.com/photos/3731256/pexels-photo-3731256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+                        CustomCard(
+                            onPressed: () {
+                              print('Ürün');
+                            },
+                            text: 'Ürün',
+                            imageUrl:
+                                'https://images.pexels.com/photos/3731256/pexels-photo-3731256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+                        CustomCard(
+                            onPressed: () {
+                              print('Ürün');
+                            },
+                            text: 'Ürün',
+                            imageUrl:
+                                'https://images.pexels.com/photos/3731256/pexels-photo-3731256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+                        CustomCard(
+                            onPressed: () {
+                              print('Ürün');
+                            },
+                            text: 'Ürün',
+                            imageUrl:
+                                'https://images.pexels.com/photos/3731256/pexels-photo-3731256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+                        CustomCard(
+                            onPressed: () {
+                              print('Ürün');
+                            },
+                            text: 'Ürün',
+                            imageUrl:
+                                'https://images.pexels.com/photos/3731256/pexels-photo-3731256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+                        CustomCard(
+                            onPressed: () {
+                              print('Ürün');
+                            },
+                            text: 'Ürün',
+                            imageUrl:
+                                'https://images.pexels.com/photos/3731256/pexels-photo-3731256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+                      ],
+                    )
                   ],
-
+                )),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      DropdownButton<String>(
+                        value: dropdownValue,
+                        icon: const Icon(Icons.arrow_downward),
+                        elevation: 16,
+                        style: const TextStyle(color: Colors.deepPurple),
+                        underline: Container(
+                          height: 2,
+                          color: colorOfMainTheme,
+                        ),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            dropdownValue = newValue!;
+                          });
+                        },
+                        items: <String>[
+                          'Ankara Teslimat Merkezi',
+                          'İstanbul Teslimat Noktası1',
+                          'İstanbul Teslimat Noktası2',
+                          'İstanbul Teslimat Noktası3'
+                        ].map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Container(
+                              width: MediaQuery.of(context).size.width / 2.6,
+                              alignment: Alignment.bottomCenter,
+                              height: 40,
+                              child: TextField(
+                                style: TextStyle(fontSize: 14),
+                                decoration: InputDecoration(
+                                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: colorOfMainTheme)) ,
+                                  hintStyle: TextStyle(fontSize: 12),
+                                  border:  OutlineInputBorder(borderSide: BorderSide(color: colorOfMainTheme)),
+                                  icon:  Icon(Icons
+                                      .calendar_today,color: colorOfMainTheme,), //icon of text field
+                                  hintText: dodString == ""
+                                      ? "Teslimat Tarihi"
+                                      : dodString.toString().substring(0, 10),
+                                ),
+                                readOnly:
+                                    true, //set it true, so that user will not able to edit text
+                                onTap: () async {
+                                  showDatePicker(
+                                          context: context,
+                                          builder: (context, child) {
+                                            return Theme(
+                                              data: Theme.of(context).copyWith(
+                                                colorScheme: ColorScheme.light(
+                                                  primary: colorOfMainTheme,
+                                                  onPrimary: Colors.white, // header text color
+                                                  onSurface:  colorOfMainTheme
+                                                ),
+                                                textButtonTheme:
+                                                    TextButtonThemeData(
+                                                  style: TextButton.styleFrom(
+                                                    primary: colorOfMainTheme
+                                                    // button text color
+                                                  ),
+                                                ),
+                                              ),
+                                              child: child!,
+                                            );
+                                          },
+                                          initialDate: DateTime.now(),
+                                          firstDate: DateTime(1900),
+                                          lastDate: DateTime(2275))
+                                      .then((value) {
+                                    setState(() {
+                                      _dateTime = value;
+                                      dodString = DateFormat('dd/MM/yyyy')
+                                          .format(_dateTime!);
+                                      dodToSend =
+                                          DateFormat('dd/MM/yyyy HH/mm/ss')
+                                              .format(_dateTime!);
+                                    });
+                                  });
+                                },
+                              )),
+                          CustomButton(
+                              onPressed: () {},
+                              child: Text('Teklif Ver'),
+                              color: colorOfMainTheme,
+                              width: 100)
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
         ),
